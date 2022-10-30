92c6a7c7169624b07f70b02d00144d0cce1cfd16
Personal Thinkpad <vangiang260694@gmail.com>
2022-10-30 17:52:45 +0700

    Fix Bug Chrome Closed immediately After Being Lauched

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+chrome = Service('C:/Users/Admin/PycharmProjects/Price_Tracker/chromedriver.exe')
+driver = webdriver.Chrome(service=chrome)
 
-global driver
+
 class Product:
 	def __init__(self, name, price_drop_notification):
 		self.name = name
 		self.price_drop_notification = price_drop_notification
 
 	def tracker_price(self):
-		chrome = Service('C:/Users/fs120806/Desktop/Document/Python OOP/Price_Tracker/chromedriver.exe')
-		driver = webdriver.Chrome(service=chrome)
 		driver.get('https://www.google.com')
+
+	def __del__(self):
+		print('The object is destroyed.')
 
 
 product = Product("nike jordan", 300000)
 product.tracker_price()
+print(type(driver))
